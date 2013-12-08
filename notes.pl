3e9228fb2d8050129ce586bdcc9936f466fd5e44
Alex Harvey <alex.harvey@puppetlabs.com>
2013-12-08 19:35:31 +1100

    Various bug fixes to notes.pl.

--- notes.pl
+++ notes.pl
@@ -6,6 +6,8 @@ use warnings;
 my $notes = $ARGV[0] ? $ARGV[0] : 'notes.txt';
 $notes eq '-h' and usage();
 
+my $courseware_dir = "$ENV{HOME}/git/courseware-fundamentals";
+
 my %data = ();
 
 # %data = (
@@ -13,9 +15,9 @@ my %data = ();
 #    'Course_Overview/Table_of_contents.md' => ...
 # );
 
-my ($slide);
+my $slide;
 
-open NOTES, "<$notes" or die "open: $!";
+open NOTES, "<$notes" or die "open: $! ($notes)";
 while (<NOTES>) {
     if ($. == 0 and !m#/.*\.md:#) {
         print "error: first line must be a slide name + ':'\n";
@@ -25,46 +27,44 @@ while (<NOTES>) {
         chomp;
         $slide = $_;
         $slide =~ s/:$//;
-        exit 1 if (!slide_exists($slide));
+        exit 1 if (!slide_exists("$courseware_dir/$slide"));
         next;
     }
-#    if ($_ eq "\n") {
-#        $data{$slide} .= "\n\n";
-#    } else {
-        $data{$slide} .= " $_";
-#    }
+    $data{$slide} .= $_;
 }
 close NOTES;
 
 # add notes to slides.
 
-foreach my $s (keys %data) {
+foreach my $slide (keys %data) {
+    my $s = "$courseware_dir/$slide";
+    print "processing '$slide' ...\n";
     system("sed '/^\.notes/d' $s >tmp.$$");
     system("mv -f tmp.$$ $s");
     system("sed '/^~~~SECTION:notes~~~/,/^~~~ENDSECTION~~~/d' $s >tmp.$$");
     system("mv -f tmp.$$ $s");
     system("echo '~~~SECTION:notes~~~' >>$s");
     open FILE, ">>$s" or die "open: $!";
-    print FILE $data{$s};
+    print FILE $data{$slide};
     close FILE;
-    system("echo '~~~ENDSECTION~~~'    >>$s");
+    system("echo '~~~ENDSECTION~~~' >>$s");
 }
 
 # subroutines.
 
 sub slide_exists {
     my $slide = shift;
-    if (! -e "./$slide") {
+    if (! -e $slide) {
         print "slide '$slide' not found, aborting\n";
-        print "(make sure this scrip is run from the\n";
-        print "courseware-fundamentals directory.)\n";
+        print "\n";
+        print "(Make sure this script is run from the courseware-fundamentals directory.)\n";
     }
-    return (-e "./$slide");
+    return (-e $slide);
 }
 
 sub usage {
     print <<EOF;
-usage: $0 <notes_file>
+usage: $0 <notes_file> [-h]
 
 Notes file format:
 
