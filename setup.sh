d6dbda89ca41bcad4b64ee6cd07a4cb4777a3a25
Alex Harvey <alex.harvey@puppetlabs.com>
2013-12-09 15:53:47 +1100

    Add setup.sh, a script to unzip VMs ready for Fundamentals class.

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+cd $HOME/VMs/
+DATE=`date +%e%b`
+mkdir puppet-master-$DATE
+mkdir puppet-client-$DATE
+mkdir puppet-ubuntu-$DATE
+unzip -d puppet-master-$DATE/ puppet-vmware.zip
+unzip -d puppet-client-$DATE/ puppet-vmware.zip
+unzip -d puppet-ubuntu-$DATE/ ubuntu-12.04.1-pe-3.0.0-vmware.zip
