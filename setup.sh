4336ca0ee26fd2c89fc2f68a6e0dee5742cb646a
Alex Harvey <alex.harvey@puppetlabs.com>
2013-12-09 18:39:32 +1100

    Add lots of stuff to setup.sh.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,48 @@
-#!/bin/bash -x
+#!/bin/bash -v -x
 cd $HOME/VMs/
 DATE=`date +%e%b |sed -e 's/ //'`
-mkdir puppet-master-$DATE
-mkdir puppet-client-$DATE
-mkdir puppet-ubuntu-$DATE
-unzip -d puppet-master-$DATE/ puppet-vmware.zip
-unzip -d puppet-client-$DATE/ puppet-vmware.zip
-unzip -d puppet-ubuntu-$DATE/ ubuntu-12.04.1-pe-3.0.0-vmware.zip
+VMRUN="/Applications//VMware Fusion.app/Contents/Library/vmrun"
+CENTOS_ZIP=puppet-vmware.zip
+UBUNTU_ZIP=ubuntu-12.04.1-pe-3.0.0-vmware.zip
+MASTER_DIR=puppet-master-$DATE
+CLIENT_DIR=puppet-client-$DATE
+UBUNTU_DIR=puppet-ubuntu-$DATE
+CENTOS_VMX=centos-6.3-pe-3.0.1.vmx
+UBUNTU_VMX=ubuntu-12.04.1-pe-3.0.0.vmx
+SHOWOFF_DIR=$HOME/git/courseware-fundamentals
+IP=$(ifconfig en0 |awk '$1 == "inet" {print $2}')
+
+# unzip images.
+mkdir $MASTER_DIR
+mkdir $CLIENT_DIR
+mkdir $UBUNTU_DIR
+unzip -d $MASTER_DIR/ $CENTOS_ZIP
+unzip -d $CLIENT_DIR/ $CENTOS_ZIP
+unzip -d $UBUNTU_DIR/ $UBUNTU_ZIP
+
+# h/w configuration for master.
+perl -pi -e '
+  s/numvcpus = "\d+"/numvcpus = "2"/; 
+  s/memsize = "\d+"/memsize = "4096"/;
+  s/ethernet0.connectionType = "nat"/ethernet0.connectionType = "bridged"/;
+' \
+  $MASTER_DIR/$CENTOS_VMX
+
+# h/w configuration for clients.
+perl -pi -e '
+  s/ethernet0.connectionType = "nat"/ethernet0.connectionType = "bridged"/
+' \
+  $CLIENT_DIR/$CENTOS_VMX \
+  $UBUNTU_DIR/$UBUNTU_VMX
+
+# upgrade and boot them.
+"$VMRUN" -T fusion upgradevm $MASTER_DIR/$CENTOS_VMX 
+"$VMRUN" -T fusion upgradevm $CLIENT_DIR/$CENTOS_VMX 
+"$VMRUN" -T fusion upgradevm $UBUNTU_DIR/$UBUNTU_VMX 
+"$VMRUN" -T fusion start $MASTER_DIR/$CENTOS_VMX 
+"$VMRUN" -T fusion start $CLIENT_DIR/$CENTOS_VMX 
+"$VMRUN" -T fusion start $UBUNTU_DIR/$UBUNTU_VMX 
+
+# start showoff.
+cd $SHOWOFF_DIR
+showoff serve -f showoff.json -h $IP
