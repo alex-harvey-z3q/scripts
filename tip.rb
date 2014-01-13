3fb6e255ea6be7a6b9ac87a8f0effb0799fd7ee2
Alex Harvey <alex.harvey@puppetlabs.com>
2014-01-13 15:48:32 -0500

    Add usage

--- tip.rb
+++ tip.rb
@@ -1,8 +1,14 @@
 #!/usr/bin/ruby
+def usage
+  puts "#{$0} <check> [<percent>]"
+  puts "e.g. $ #{$0} 10.31 0.25 # calculate 25% tip on 10.31"
+  exit 1
+end
 default_tip = 0.2
 check = ARGV[0]
+usage if !check
 percent = ARGV[1] || default_tip
-total = (check.to_f * (percent.to_f + 1)).ceil.round(2)
+total = (check.to_f * (percent.to_f + 1)).ceil
 tip = (total - check.to_f).round(2)
 puts "tip = #{tip}"
 puts "total = #{total}"
