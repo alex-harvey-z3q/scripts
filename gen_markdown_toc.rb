3586036ca7d80e817a8cc6e88b5fbea19af6b116
Alex Harvey <alexharv074@gmail.com>
2018-08-27 00:33:13 +1000

    Clean up gen_markdown_toc.rb

--- gen_markdown_toc.rb
+++ gen_markdown_toc.rb
@@ -1,38 +1,45 @@
+#!/usr/bin/env ruby
+
 def usage
-  puts "Usage: $0 FILE.md"
+  puts "Usage: #{$0} FILE.md"
   exit 1
+end
+
+def header_to_ref(string)
+  string
+    .gsub(/ /, "-")
+    .gsub(/[\.\/,&\()<>-]+/, "-")
+    .gsub(/-$/, "")
+    .downcase
 end
 
 usage unless ARGV.length == 1
 source_file = ARGV[0]
 
-def header_to_string(s)
-  s.gsub(/ /, '-').downcase.gsub(/[\.\/,&\()<>-]+/, '-').gsub(/-$/,'')
-end
+puts "#### Table of contents\n\n"
 
-c = 1 ; File.open(source_file).each_line do |line|
+c=1; File.open(source_file).each_line do |line|
+  next unless line.match(/^#/)
 
-  base = File.basename(source_file)
+  level, header = line.match(/^(#+) (.*)/).captures
+  next if header == "Table of contents"
 
-  if line.match(/^#/)
+  # I have assumed only 2nd, 3rd & 4th level headers
+  # belong in the generated ToC.
+  #
+  next unless ["##", "###", "####"].include?(level)
 
-    level, header = line.match(/^(#+) (.*)/).captures
-    ref = header_to_string(header)
+  ref = header_to_ref(header)
 
-    case level
-    when '#'
-      next
-    when '##'
-      start = "#{c}. "
-      c += 1
-    when '###'
-      start = "    - "
-    when '####'
-      start = "        * "
-    end
-
-    puts "#{start}[#{header}](#{base}" + '#' + "#{ref})"
+  case level
+  when "##"
+    start = "#{c}."
+    c += 1
+  when "###"
+    start = "    -"
+  when "####"
+    start = "        *"
   end
-end
 
-# require 'pry' ; binding.pry
+  puts "#{start} [#{header}](##{ref})"
+end
