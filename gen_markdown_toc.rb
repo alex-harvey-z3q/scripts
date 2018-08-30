2ff50b957c896ed600fa96997f873c075dd6e63f
Alex Harvey <alexharv074@gmail.com>
2018-08-30 10:38:41 +1000

    Refactor gen_markdown_toc.rb

--- gen_markdown_toc.rb
+++ gen_markdown_toc.rb
@@ -5,41 +5,61 @@ def usage
   exit 1
 end
 
-def header_to_ref(string)
-  string
-    .gsub(/ /, "-")
-    .gsub(/[\.\/,&\()<>-]+/, "-")
-    .gsub(/-$/, "")
-    .downcase
+class ToCWriter
+  def initialize(source_file, top=2, max=4)
+    @source_file = source_file
+    @top = top
+    @max = max
+    @count = 1
+    @level  = ""
+    @header = ""
+    @start  = ""
+  end
+
+  def write
+    puts "#### Table of contents\n\n"
+
+    File.open(@source_file).each_line do |line|
+      next unless line.match(/^#/)
+
+      @level, @header = line.match(/^(#+) *(.*)/).captures
+      next if ignore_this_header?
+
+      ref = header_to_ref
+      set_start
+
+      puts "#{@start} [#{@header}](##{ref})"
+    end
+  end
+
+ private
+
+  def ignore_this_header?
+    @header == "Table of contents" || \
+      @level.length < @top || \
+      @level.length > @max
+  end
+
+  def header_to_ref
+    @header
+      .gsub(/[^a-zA-Z\d]+/, "-")
+      .gsub(/-$/, "")
+      .downcase
+  end
+
+  def set_start
+    len = @level.length
+    if len == @top
+      @start = "#{@count}."
+      @count += 1
+    else
+      bullet = len % 2 == 0 ? "-" : "*"
+      @start = "    " * (len - @top) + bullet
+    end
+  end
 end
 
 usage unless ARGV.length == 1
 source_file = ARGV[0]
 
-puts "#### Table of contents\n\n"
-
-c=1; File.open(source_file).each_line do |line|
-  next unless line.match(/^#/)
-
-  level, header = line.match(/^(#+) (.*)/).captures
-  next if header == "Table of contents"
-
-  # I have assumed only 2nd, 3rd & 4th level headers
-  # belong in the generated ToC.
-  #
-  next unless ["##", "###", "####"].include?(level)
-
-  ref = header_to_ref(header)
-
-  case level
-  when "##"
-    start = "#{c}."
-    c += 1
-  when "###"
-    start = "    -"
-  when "####"
-    start = "        *"
-  end
-
-  puts "#{start} [#{header}](##{ref})"
-end
+ToCWriter.new(source_file).write
